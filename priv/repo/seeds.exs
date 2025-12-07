60fea236898e2bd277782117736cba2589c7ce65
Tarun Verghis <tarun.verghis@gmail.com>
2025-12-07 08:29:21 -0800

    Add some videos in repo seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,19 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+YodelStudio.Repo.insert!(%YodelStudio.Catalog.Video{
+  title: "2.5+ Hours of Boppin NES Music",
+  slug: "J3w0acXJeRE",
+  active: true,
+  channel_name: "Balticsalmon",
+  channel_id: "UCRAtYRe8tPynWadDbAu30TQ"
+})
+
+YodelStudio.Repo.insert!(%YodelStudio.Catalog.Video{
+  title: "SNES Music Nostalgia Mix",
+  slug: "Flm4byJT5og",
+  active: true,
+  channel_name: "Mysterious Zim",
+  channel_id: "UCb86Gv7SIIoiKDSIrpEDQQQ"
+})
