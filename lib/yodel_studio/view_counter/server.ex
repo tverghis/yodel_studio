53152cc32b6fd68b37a9733113b2284967ff00af
Tarun Verghis <tarun.verghis@gmail.com>
2025-12-06 19:26:20 -0800

    Add public interface for getting total views

--- lib/yodel_studio/view_counter/server.ex
+++ lib/yodel_studio/view_counter/server.ex
@@ -2,6 +2,14 @@ defmodule YodelStudio.ViewCounter.Server do
   use GenServer
   alias YodelStudio.Catalog
 
+  # Public interface
+  # -------------------
+  def get_total_views() do
+    GenServer.call(__MODULE__, :get_total_views)
+  end
+
+  # GenServer machinery
+  # -------------------
   def start_link(_) do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
@@ -9,12 +17,17 @@ defmodule YodelStudio.ViewCounter.Server do
   @impl true
   def init(_) do
     :timer.send_interval(:timer.seconds(5), :stats)
-    {:ok, nil}
+    {:ok, %{total_views: 0}}
   end
 
   @impl true
   def handle_info(:stats, state) do
     Catalog.list_videos() |> Enum.each(fn video -> IO.puts("#{video.title}") end)
     {:noreply, state}
+  end
+
+  @impl true
+  def handle_call(:get_total_views, _, %{total_views: total_views} = state) do
+    {:reply, total_views, state}
   end
 end
