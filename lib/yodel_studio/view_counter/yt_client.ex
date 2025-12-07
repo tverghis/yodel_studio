3968b796cbd190febaff219e9ad30991e0182289
Tarun Verghis <tarun.verghis@gmail.com>
2025-12-06 23:54:40 -0800

    Add YouTube client to fetch view counts

---
+++ lib/yodel_studio/view_counter/yt_client.ex
@@ -0,0 +1,43 @@
+defmodule YodelStudio.ViewCounter.YtClient do
+  require Logger
+
+  def get_view_counts(video_ids) do
+    case Req.get(construct_url(video_ids)) do
+      {:ok, resp} ->
+        handle_resp(resp)
+
+      {:error, exception} ->
+        Logger.error(Exception.message(exception))
+        {:error, :api_error}
+    end
+  end
+
+  defp construct_url(video_ids) do
+    ids = "id=" <> Enum.join(video_ids, ",")
+    part = "&part=statistics"
+    fields = "&fields=items/statistics/viewCount"
+    key = "&key=" <> Application.fetch_env!(:yodel_studio, :yt_api_key)
+
+    "https://www.googleapis.com/youtube/v3/videos?" <> ids <> part <> fields <> key
+  end
+
+  defp aggregate_counts(api_response) do
+    api_response
+    |> Enum.map(fn item ->
+      item["statistics"]["viewCount"]
+      |> String.to_integer()
+    end)
+    |> Enum.sum()
+  end
+
+  defp handle_resp(%{status: status, body: body}) when div(status, 100) == 2 do
+    total_counts = aggregate_counts(body["items"])
+    Logger.debug("Fetched counts: #{total_counts}")
+    {:ok, total_counts}
+  end
+
+  defp handle_resp(%{status: status}) do
+    Logger.error("YouTube API returned non-success status #{status}")
+    {:error, :api_error}
+  end
+end
